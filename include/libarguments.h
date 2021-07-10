0ee9d5cb8ecad71811809d8aedc128a3c3504ca5
李诗军 <foxintango@yeah.net>
2021-07-10 20:53:20 +0800

    初始化

---
+++ include/libarguments.h
@@ -0,0 +1,10 @@
+#ifndef _LIB_ARGUMENTS_H_
+#define _LIB_ARGUMENTS_H_
+
+#include "arguments.h"
+
+namespace foxintango {
+
+}
+
+#endif
---
+++ main/main.cpp
@@ -0,0 +1,8 @@
+#include <libarguments.h>
+
+using namespace foxintango;
+
+int main(int argc,char** argv) {
+    arguments args(argc,argv);
+    return 0;
+}
