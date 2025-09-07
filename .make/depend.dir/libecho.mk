80546c3b4c0d2e24fa45cf8440830d5801d32d2b
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 17:59:23 +0800

    Sun Sep  7 05:59:23 PM CST 2025

--- .make/depend.dir/libecho.mk
+++ .make/depend.dir/libecho.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libecho.recursive
 ECHO_TARGETS += libecho.echo
 libecho.recursive:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libarguments/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libarguments/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/ && make recursive && make install
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libarguments/               >> /home/lidali/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libarguments/.make >> /home/lidali/alpine/libraries/libecho/.make/super
+	cd /home/lidali/alpine/libraries/libecho/ && make recursive && make install
+	-rm /home/lidali/alpine/libraries/libecho/.make/super
 libecho.echo:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libarguments/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libarguments/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/ && make echo
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libarguments/               >> /home/lidali/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libarguments/.make >> /home/lidali/alpine/libraries/libecho/.make/super
+	cd /home/lidali/alpine/libraries/libecho/ && make echo
+	-rm /home/lidali/alpine/libraries/libecho/.make/super
